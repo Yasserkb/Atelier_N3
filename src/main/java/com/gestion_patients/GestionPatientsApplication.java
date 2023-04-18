846f6b07daa9886c72ee5ed742fa767be7b3095d
Yasserkb <koubachi.yasser@gmail.com>
2023-04-18 12:46:29 +0000

    adding spring security

--- src/main/java/com/gestion_patients/GestionPatientsApplication.java
+++ src/main/java/com/gestion_patients/GestionPatientsApplication.java
@@ -2,10 +2,14 @@ package com.gestion_patients;
 
 import com.gestion_patients.entities.Patients;
 import com.gestion_patients.repositories.PatientsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Date;
 
@@ -16,21 +20,25 @@ public class GestionPatientsApplication {
         SpringApplication.run(GestionPatientsApplication.class, args);
     }
 
-    @Bean
+
+    //@Bean
     CommandLineRunner commandLineRunner(PatientsRepository patientsRepository){
         return  args -> {
             patientsRepository.save(new Patients(null,"ahmed",new Date(),true,251));
-            patientsRepository.save(new Patients(null,"Yasser",new Date(),false,01));
-            patientsRepository.save(new Patients(null,"Youssef",new Date(),false,02));
+            patientsRepository.save(new Patients(null,"Yasser",new Date(),false,124));
+            patientsRepository.save(new Patients(null,"Youssef",new Date(),false,202));
             patientsRepository.save(new Patients(null,"Badr",new Date(),true,510));
 
 
-            patientsRepository.findAll().forEach(p->{
-                System.out.println(p.getName());
-            });
-
-
+//            patientsRepository.findAll().forEach(p->{
+//                System.out.println(p.getName());
+//            });
         };
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
--- src/main/java/com/gestion_patients/web/PatientController.java
+++ src/main/java/com/gestion_patients/web/PatientController.java
@@ -3,15 +3,20 @@ package com.gestion_patients.web;
 
 import com.gestion_patients.entities.Patients;
 import com.gestion_patients.repositories.PatientsRepository;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.naming.Binding;
 import java.util.List;
 
 @Controller
@@ -19,7 +24,7 @@ import java.util.List;
 public class PatientController {
     private PatientsRepository patientsRepository;
 
-    @GetMapping(path = "/index")
+    @GetMapping(path = "/user/index")
     public String patients(Model model,
                            @RequestParam(name = "page",defaultValue = "0") int page,
                            @RequestParam(name = "size",defaultValue = "5") int size,
@@ -33,21 +38,54 @@ public class PatientController {
         return "patient";
     }
 
-    @GetMapping("/delete")
+    @GetMapping("/admin/delete")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String delete(Long id, String keyword, int page){
         patientsRepository.deleteById(id);
-        return "redirect:/index?page="+page+"&keyword="+keyword;
+        return "redirect:/user/index?page="+page+"&keyword="+keyword;
     }
 
     @GetMapping("/")
     public String home(){
-        return "redirect:/index";
+        return "redirect:/user/index";
     }
 
-    @GetMapping("/patients")
+    @GetMapping("/admin/patients")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @ResponseBody
     public List<Patients> listPastients(){
         return patientsRepository.findAll();
     }
+
+
+    @GetMapping("/admin/formPatients")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public String formPatients(Model model){
+        model.addAttribute("patients", new Patients());
+        return "formPatients";
+    }
+
+    @PostMapping("/admin/save")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public String save(Model model, @Valid Patients patients, BindingResult bindingResult,
+                       @RequestParam(defaultValue = "") String keyword,
+                       @RequestParam(defaultValue = "0") int  page){
+        if (bindingResult.hasErrors()) return "formPatients";
+        patientsRepository.save(patients);
+        return "redirect:/user/index?page="+page+"&keyword="+keyword;
+    }
+
+
+    @GetMapping("/admin/edit")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public String editPatients(Model model, Long id, String keyword, int page){
+        Patients patients = patientsRepository.findById(id).orElse(null);
+        if (patients == null) throw new RuntimeException("no patients with that ID");
+        model.addAttribute("patients", patients);
+        model.addAttribute("page", page);
+        model.addAttribute("keyword", keyword);
+        return "editPatients";
+    }
+
 
 }
